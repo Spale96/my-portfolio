74f3d55ecfcd8a6c5a61c337a691b3fcceda9e00
Spale96 <mihajlospasicyolo@gmail.com>
2023-06-13 01:15:12 +0200

    Update Navbar.jsx
    Added passive on scroll eventListener.

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -21,10 +21,10 @@ export default function Navbar() {
             }
         };
 
-        window.addEventListener('scroll', handleScroll);
+        window.addEventListener('scroll', handleScroll, { passive: true });
 
         return () => {
-            window.removeEventListener('scroll', handleScroll);
+            window.removeEventListener('scroll', handleScroll, { passive: true });
         };
     }, []);
 
